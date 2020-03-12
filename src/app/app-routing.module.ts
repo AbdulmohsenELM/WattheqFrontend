729086b14184137fee99a1b71a552c63a7cd6f01
abdalsahli <abdalsahli@elm.sa>
2020-03-12 12:52:42 +0300

    HEADER AND FOOTER AS COMPONENTS

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,13 +3,17 @@ import { Routes, RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { RegistrationComponent } from './registration/registration.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
 
 
 const routes: Routes = [
   {path: '', component: AppComponent},
   {path: 'home', component: AppComponent},
   {path: 'login', component: LoginComponent},
-  {path: 'registration', component: RegistrationComponent}
+  {path: 'registration', component: RegistrationComponent},
+  {path: 'header', component: HeaderComponent},
+  {path: 'footer', component: FooterComponent}
 ];
 
 @NgModule({
